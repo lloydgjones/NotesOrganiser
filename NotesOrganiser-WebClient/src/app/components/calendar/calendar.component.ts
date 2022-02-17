e1ed682b4b0d3d05aac9b7cd33bc4051efea2433
Lloyd G. Jones <lloydgjones@tutanota.com>
2022-02-17 17:30:12 +0000

    Add basic calendar view functionality

--- NotesOrganiser-WebClient/src/app/components/calendar/calendar.component.ts
+++ NotesOrganiser-WebClient/src/app/components/calendar/calendar.component.ts
@@ -1,6 +1,10 @@
+import { Note } from 'src/app/models/note/note.model';
+import { Task } from 'src/app/models/task/task.model';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CalendarOptions } from '@fullcalendar/angular';
+import { NoteService } from 'src/app/services/note/note.service';
+import { TaskService } from 'src/app/services/task/task.service';
 import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
@@ -11,16 +15,27 @@ import { UserService } from 'src/app/services/user/user.service';
 export class CalendarComponent implements OnInit {
   data: any;
   email: String;
+  notes: any;
+  tasks: any;
+  events: any;
 
   calendarOptions: CalendarOptions = {
+    nextDayThreshold: '02:00:00',
     initialView: 'dayGridMonth',
+    displayEventTime: true,
     aspectRatio: 2.8,
     buttonText: {
       today: 'Today'
+    },
+    eventTimeFormat: {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+      meridiem: false
     }
   };
 
-  constructor(private _router: Router, private _user: UserService) { }
+  constructor(private _router: Router, private _user: UserService, private noteService: NoteService, private taskService: TaskService) { }
 
   ngOnInit(): void {
     this._user.getUser()
@@ -32,5 +47,56 @@ export class CalendarComponent implements OnInit {
 
   getAccount(data){
     this.email = data.email;
+    this.getNoteData();
+  }
+
+  noteToEvent(note : Note){
+    return {
+      title: note.name,
+      start: note.time.toLocaleString().replace('Z', '')
+    };
+  }
+
+  taskToEvent(task : Task){
+    return {
+      title: task.name,
+      start: task.time.toLocaleString().replace('Z', '')
+    };
+  }
+
+  // TODO: Item Functions
+  getNoteData() {
+    this.noteService.getDataByUser(this.email).subscribe(
+      res => {
+        this.notes = Object.keys(res).map(index => { let note = res[index];
+          if(note.time != null) { return this.noteToEvent(note); }
+          else { return; }
+        });
+
+        this.notes = this.notes.filter(function(note) {
+          return note !== undefined;
+        });
+
+        this.events = this.notes;
+        this.getTaskData();
+      }
+    );
+  }
+
+  getTaskData() {
+    this.taskService.getDataByUser(this.email).subscribe(
+      res => {
+        this.tasks = Object.keys(res).map(index => { let task = res[index];
+          if(task.time != null) { return this.taskToEvent(task); }
+          else { return; }
+        });
+
+        this.tasks = this.tasks.filter(function(task) {
+          return task !== undefined;
+        });
+
+        this.calendarOptions.events = this.events.concat(this.tasks);
+      }
+    );
   }
 }
